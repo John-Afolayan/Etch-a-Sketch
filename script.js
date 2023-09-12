a8fe6a6e40cab1ad69f9ea6726d915ff8ac844e2
John A <johnafolayan11@yahoo.ca>
2023-09-12 01:48:00 -0400

    Made layout more responsive

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const sketch_frame = document.querySelector("#sketch_frame");
 
 function generateCells(column, row){
+    const sketchFrameSize = sketch_frame.getBoundingClientRect().width;
+    const cellSize = sketchFrameSize / column;
+
+    document.documentElement.style.setProperty('--cell-size', `${cellSize}px`);
+
     document.documentElement.style.setProperty('--grid-col', column);
     document.documentElement.style.setProperty('--grid-row', row);
     for(let i=1; i <= column; i++) {
@@ -19,7 +24,7 @@ function generateCells(column, row){
     }
 }
 
-generateCells(100, 15);
+generateCells(15, 15);
 
 function getRandomColor() {
     return Math.floor(Math.random() * 16777215).toString(16);
